331f5b61d9d3c9b09eedf6e558ba11e6df2617bc
joses87 <86647413+joses87@users.noreply.github.com>
2021-06-30 10:56:19 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+//import Phaser from './phaser.js';
+
+
+
+import PlayScene from './scenes/PlayScene.js';
+import MenuScene from './scenes/MenuScene.js';
+import PreloadScene from './scenes/PreloadScene.js';
+import ScoreScene from './scenes/ScoreScene.js';
+import PauseScene from './scenes/PauseScene.js';
+
+const WIDTH = 400;
+const HEIGHT = 600;
+const BIRD_POSITION = {  x : WIDTH * 0.1, y: HEIGHT / 2 }
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
+}
+
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene  ];
+const createScene = Scene => new Scene(SHARED_CONFIG)
+const initScenes = ()=> Scenes.map(createScene);
+
+const config = {
+  type: Phaser.AUTO, //WebGL (Web Graphics Library)
+  ...SHARED_CONFIG,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',     //Arcade Physics plugin - manages physics simulation
+    arcade: {
+      // gravity: { y: 400 },
+      // debug: true
+    }
+  },
+  scene: initScenes()
+}
+
+new Phaser.Game(config);
